4e73f9fad4d7db8851f5c6b3ce1082df67fda388
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-11 13:53:36 +0100

    routing

--- src/app/features/settings/overview/overview.component.ts
+++ src/app/features/settings/overview/overview.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { StatisticsComponent } from "../statistics/statistics.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [],
+  imports: [StatisticsComponent, RouterOutlet, RouterLink],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
-
+  showStatistics = false
+  
+  loadStatistics() {
+    this.showStatistics = true
+  }
 }
--- src/app/features/settings/settings.routes.ts
+++ src/app/features/settings/settings.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from "@angular/router";
 export const SettingsRoutes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./overview/overview.component').then((component) => component.OverviewComponent)
+        loadComponent: () => import('./overview/overview.component').then((component) => component.OverviewComponent),
+        children: [
+            {
+                path: 'panel',
+                loadComponent: () => import('./panel/panel.component').then((component) => component.PanelComponent),
+            },
+            {
+                path: 'statistics',
+                loadComponent: () => import('./statistics/statistics.component').then((component) => component.StatisticsComponent),
+            }
+        ]
     }
 ]
