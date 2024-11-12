0e3cb8517258db55b4df07fc132526ec231c7819
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-12 11:04:49 +0100

    list

---
+++ src/app/features/photos/photos.routes.ts
@@ -0,0 +1,8 @@
+import { ListComponent } from "./list/list.component";
+
+export const PhotosRoutes = [
+    {
+        path: '',
+        loadComponent: () => import('./list/list.component').then((cmp) => cmp.ListComponent),
+    }
+]
