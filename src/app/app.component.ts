95028b4bd7210806f82a8745f1786a2bf93296a1
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-18 13:11:15 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-bosch';
+  members = [
+    {
+      id: "uuid1",
+      name: 'Petr'
+    },
+    {
+      id: "uuid2",
+      name: 'Karel'
+    }
+  ]
+
+  trackByMemberId(index: number, member: any) {
+    return member.id
+  }
 }
